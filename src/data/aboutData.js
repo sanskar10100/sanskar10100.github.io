7439cb33420ca36f404494eabe9ed2fa32a325b4
Sanskar Agrawal <sanskar10100@gmail.com>
2022-01-01 18:28:22 +0530

    Updates

--- src/data/aboutData.js
+++
@@ -1,6 +0,0 @@
-export const aboutData = {
-    title: "Who I am",
-    description1: "I am Sanskar, an Android Engineer who occassionally dabbles in the backend and the cloud.",
-    description2: "I'm currently helping Ikanshy build their retail checkout optimization platform.",
-    image: 2
-}
--- src/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'sanskar10100@gmail.com',
-    phone: '+91 7499229768',
-    address: 'Samne Ghat, Varanasi, Uttar Pradesh',
-
-    sheetAPI: 'https://sheetdb.io/api/v1/zbyx9hvzqqhqr'
-}
