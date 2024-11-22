7111aab51ce878e8bd4a353c31c0dbda62831562
Sanskar Agrawal <sanskar10100@gmail.com>
2024-11-22 22:41:19 +0530

    add business card game code

---
+++ business/precache-manifest.f91d2c1165703e28810f60dfe95e644b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b8407316ac500b1f492",
+    "url": "/css/app.73341c6f.css"
+  },
+  {
+    "revision": "8c3e56a7f0ad7a7fdffc7d3b5538d014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b8407316ac500b1f492",
+    "url": "/js/app.cc260c46.js"
+  },
+  {
+    "revision": "fa5403a58d93005e50fb",
+    "url": "/js/chunk-vendors.1f3e9fe8.js"
+  },
+  {
+    "revision": "b4bc735e786c08b459d27e93b48867f9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2c4103ea0267a6d7b0dd48df055d5e89",
+    "url": "/scan.mp3"
+  }
+]);
